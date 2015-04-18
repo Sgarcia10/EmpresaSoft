d8cf868c1861e3f08c9845e47430c238567b82a2
Sgarcia10 <garciasantiago10@gmail.com>
2015-04-18 11:12:19 -0500

    Cambio1

--- source/prueba.java
+++ source/prueba.java
@@ -6,7 +6,12 @@ import java.util.Locale;
 
 public class prueba 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
+		
+		
+		
 		Date fechaActual =new Date();
 		SimpleDateFormat format = new SimpleDateFormat("yyyyMMMMEEEE", Locale.US);
 		System.out.println(""+format.format(fechaActual));
