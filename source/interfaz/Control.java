601c9577ae48e1fa799112af6c5776617eecf0e8
catrujillo3180 <ca.trujillo3180@gmail.com>
2015-06-20 02:54:52 -0500

    darPeriodo()

--- source/interfaz/Control.java
+++ source/interfaz/Control.java
@@ -357,7 +357,7 @@ public class Control {
 		return rta;
 	}
 
-	public ArrayList getListaDeduccionesPrestamos() {
+	public ArrayList getListaDeduccionesPrestamos(String periodo) {
 		// TODO Auto-generated method stub
 		ArrayList rta = new ArrayList( );
 		return rta;
